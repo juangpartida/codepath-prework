fde924da58e1cd5dc46ddb467ab98cbf7d21c071
juangpartida <juangpartida@protonmail.com>
2022-02-03 22:51:49 -0800

    Commit #1
    Added prework files to github repo

---
+++ Prework - Intro to Mobile Development/Prework - Intro to Mobile Development/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Prework - Intro to Mobile Development
+//
+//  Created by Juan G. Partida on 2/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("Hello")
+        TextLabel.textColor = UIColor.orange
+    }
+}
+
